e830b25482470ecddd3209dbdaa77f90c8a1b304
Wael M. Nasreddine <wael@getpublica.com>
2018-05-05 16:22:12 -0700

    fix it

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -134,7 +134,7 @@ RUN apt-get -y install libgconf-2-4 \
 fi
 
 echo "# install Go
-RUN apt-get -y install -t golang-1.10"
+RUN apt-get -y install golang-1.10"
 
 echo "# install Bazel
 RUN curl --location --compressed https://github.com/bazelbuild/bazel/releases/download/0.13.0/bazel-0.13.0-installer-linux-x86_64.sh > /tmp/bazel-0.13.0-installer-linux-x86_64.sh \
