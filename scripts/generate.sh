271d4c99c1a5afad545e9af9cab47cab9917ac6b
Wael M. Nasreddine <wael@getpublica.com>
2018-05-05 17:06:04 -0700

    make sure gopath/bin exists

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -136,11 +136,13 @@ fi
 echo "# install Go
 RUN apt-get -y install golang-1.10
 
-ENV PATH=\$PATH:/usr/lib/go-1.10/bin
+ENV GOPATH=/root/go PATH=\$PATH:/usr/lib/go-1.10/bin:/go/bin
 
 RUN curl -LO https://raw.githubusercontent.com/golang/dep/master/install.sh \
 	&& chmod +x install.sh \
-	&& ./install.sh"
+	&& ./install.sh \
+	&& mkdir -p /root/go/bin \
+	&& mkdir -p /root/go/src"
 
 echo "# install Bazel
 RUN curl --location --compressed https://github.com/bazelbuild/bazel/releases/download/0.13.0/bazel-0.13.0-installer-linux-x86_64.sh > /tmp/bazel-0.13.0-installer-linux-x86_64.sh \
